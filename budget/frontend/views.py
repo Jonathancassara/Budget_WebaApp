992d7a5065fc7e66e29d20d3451c287b335cfc95
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-14 00:08:07 +0100

    init Django
    Install Django et init
    page Index + js + css  + dossier  js css  images

---
+++ budget/frontend/views.py
@@ -0,0 +1,5 @@
+# frontend/views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'frontend/index.html')
---
+++ budget/static/js/script.js
@@ -0,0 +1,4 @@
+// static/js/script.js
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Bouton cliqué !');
+});
