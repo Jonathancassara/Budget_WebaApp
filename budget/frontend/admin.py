96da75edfd58177aa1e99df6d90ce2ed4a9b0e99
JONATHAN CASSARA <37684306+Jonathancassara@users.noreply.github.com>
2024-12-14 01:43:37 +0100

    create database modele
    create database modele
    fields=[
                    ('id', models.AutoField(primary_key=True, serialize=False)),
                    ('nom', models.CharField(max_length=255, verbose_name='Nom')),
                    ('commentaire', models.TextField(blank=True, verbose_name='Commentaire')),
                    ('date', models.DateField(verbose_name='Date')),
                    ('montant', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Montant')),
                    ('devise', models.CharField(choices=[('EURO', 'Euro'), ('PLN', 'Polish Zloty'), ('USD', 'US Dollar')], max_length=4, verbose_name='Devise')),
                    ('solde', models.CharField(choices=[('CREDIT', 'Credit'), ('DEBIT', 'Debit')], max_length=6, verbose_name='Solde')),

--- budget/frontend/admin.py
+++ budget/frontend/admin.py
@@ -1,3 +1,9 @@
+# frontend/admin.py
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'montant', 'devise', 'date', 'solde')
+    list_filter = ('devise', 'solde', 'date')
+    search_fields = ('nom', 'commentaire')
